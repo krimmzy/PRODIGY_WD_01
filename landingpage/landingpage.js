956b4569eed6f4a88e41a60235a967639d301a8b
krimmzy <125971535+krimmzy@users.noreply.github.com>
2024-09-16 12:40:16 +0100

    Add files via upload
    first

---
+++ landingpage/landingpage.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.querySelector('.btn-primary');
+    btn.addEventListener('click', function() {
+        alert('Thank you for clicking the button!');
+    });
+});
